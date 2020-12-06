74a8981844c2610833b182a0188ea3f7c0b3391b
Punithkumar18 <75051555+Punithkumar18@users.noreply.github.com>
2020-12-06 18:28:26 +0530

    final commit

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("Hello ! This is Javascript .")
+let btnTranslate = document.querySelector("#btn-translate");
+let txtInput = document.querySelector("#txt-input");
+let outputDiv = document.querySelector("#output");
+
+
+
+let serverURL = "https://api.funtranslations.com/translate/doge.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+
+}
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server ! try again after some time")
+}
+function clickHandler(){
+
+    let inputText = txtInput.value;
+
+fetch(getTranslationURL(inputText))
+
+    .then(response => response.json())
+    .then(json => {
+
+        let translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    } )
+    .catch(erorHandler)
+
+
+
+};
+
+btnTranslate.addEventListener("click",clickHandler);
